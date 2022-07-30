2381f933d9b6122defb274d691fdeab7b8147028
Lalá Nametala <lalanametala@gmail.com>
2022-07-30 17:19:29 -0300

    :sparkles: feat(Benefits Section): benefits data

---
+++ src/components/benefits/utils/helpers.js
@@ -0,0 +1,29 @@
+import coupon from './images/coupon.svg';
+import giftCoupon from './images/gift-coupon.svg';
+import moneyBag from './images/money-bag.svg';
+import truck from './images/truck.svg';
+
+export const benefits = [
+  {
+    pContent: '15% OFF em todas as compras na Wine',
+    imgAlt: 'coupon',
+    imgSrc: coupon,
+  },
+  {
+    pContent: 'Brindes e cupons no Indique Amigos',
+    imgAlt: 'gift and coupon',
+    imgSrc: giftCoupon,
+  },
+  {
+    pContent: '3x mais cashback pelo WineUP',
+    imgAlt: 'money bag',
+    imgSrc: moneyBag,
+  },
+  {
+    pContent: 'Taxa de entrega diferenciada',
+    imgAlt: 'truck',
+    imgSrc: truck,
+  },
+];
+
+export default benefits;
