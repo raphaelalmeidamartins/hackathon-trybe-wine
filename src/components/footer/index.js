087d90a1feef202f0f8f5eeb7897a3502366353c
Lalá Nametala <lalanametala@gmail.com>
2022-07-30 18:55:46 -0300

    :recycle: refactor(footer): semantics fix

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,23 +4,23 @@ import wineWLogo from './utils/images/wine-w-logo.svg';
 
 function Footer() {
   return (
-    <Container component="footer" columns={{ xs: 4, sm: 8, md: 12 }} maxWidth="lg">
-      <Grid container>
+    <Container component="footer" maxWidth="lg">
+      <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
         <Grid item xs={0} sm={0} md={6}>
-          <Box>
+          <Box component="section">
             <img alt="wine w logo" src={wineWLogo} />
           </Box>
-          <Box>
+          <Box component="section">
             <p>W2W E-Commerce de Vinhos S.A</p>
             <p>CNPJ: 09.813.204/0002-05</p>
           </Box>
-          <Box>
+          <Box component="section">
             <p>Rua Comendador Alcides Simão Helou, 1478</p>
             <p>Serra - ES - CEP 29168-090</p>
           </Box>
         </Grid>
         <Grid item xs={4} sm={8} md={6}>
-          <Box>
+          <Box component="section">
             <p>
               SE BEBER, NÃO DIRIJA. APRECIE COM MODERAÇÃO. A VENDA DE BEBIDAS ALCOÓLICAS É PROIBIDA
               PARA MENORES DE 18 ANOS.
