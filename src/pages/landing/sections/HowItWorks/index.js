c6a93e8ef7f0110ae96e69bfce93cd6e0ef70135
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-31 08:46:00 -0300

    :lipstick: style(HowItWorks section): change icons

--- src/pages/landing/sections/HowItWorks/index.js
+++ src/pages/landing/sections/HowItWorks/index.js
@@ -3,17 +3,14 @@ import * as React from 'react';
 import HowItWorksCard from '../../../../components/HowItWorksCard';
 import StepsGrid from './style';
 import steps from './utils/helpers';
-import theme from '../../../../styles/theme';
 
 export default function HowItWorks() {
-  const { colors } = theme;
-
   return (
     <section style={{ margin: '48px 0' }}>
       <Typography variant="h3" component="h1" align="center" gutterBottom>
         Entenda como funciona
       </Typography>
-      <Typography align="center" paragraph sx={{ color: colors.burnetOrange }}>
+      <Typography align="center" paragraph>
         No Clube Wine você recebe, todo mês, 2 rótulos escolhidos a dedo das
         melhores vinícolas do mundo!
       </Typography>
