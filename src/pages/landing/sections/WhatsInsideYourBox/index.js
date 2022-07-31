80cee8cb5b9619bc695edb7877d8da4fb647a3f8
Erik-EFL <erik.lima.f@outlook.com>
2022-07-31 04:25:55 -0300

    feat(WhatIsInsideABox): :sparkles: Stiling the page What is in the box

--- src/pages/landing/sections/WhatsInsideYourBox/index.js
+++ src/pages/landing/sections/WhatsInsideYourBox/index.js
@@ -1,4 +1,4 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 import WhatsInsideBoxCard from '../../../../components/WhatsInsideBoxCard';
 import wineBottles from './images/wineBottles.png';
@@ -7,8 +7,15 @@ import dropper from './images/dropper.png';
 
 function WhatsInsideYourBox() {
   return (
-    <Container component="section">
-      <h2>O que vai dentro da sua WineBox?</h2>
+    <Container
+      component="section"
+      sx={{
+        marginTop: '30px',
+        marginBotton: '50px',
+        padding: '0 5px',
+      }}
+    >
+      <Typography variant="h5" align="center" component="h3" gutterBottom>O que vai dentro da sua WineBox?</Typography>
       <Grid container columns={{ xs: 4, sm: 6, md: 12 }}>
         <Grid item xs={4} sm={2} md={4}>
           <WhatsInsideBoxCard
