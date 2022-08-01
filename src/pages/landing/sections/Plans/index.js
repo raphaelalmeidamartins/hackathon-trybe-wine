69ac6604da1f9eacd33f2034624b061614a48657
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-08-01 20:38:50 -0300

    :lipstick: style: change plan heading

--- src/pages/landing/sections/Plans/index.js
+++ src/pages/landing/sections/Plans/index.js
@@ -1,4 +1,5 @@
 import { useTheme } from '@emotion/react';
+import { Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Hidden from '@mui/material/Hidden';
@@ -13,7 +14,9 @@ import PlansContainerBox from './style';
 function Plans() {
   const dispatch = useDispatch();
   const subscription = useSelector((state) => state.subscription);
-  const { colors: { gradient } } = useTheme();
+  const {
+    colors: { gradient },
+  } = useTheme();
 
   useEffect(() => {
     dispatch(fetchSubscriptionData());
@@ -23,34 +26,44 @@ function Plans() {
     <PlansContainerBox gradient={gradient}>
       <Container component="section">
         {subscription?.plans && (
-        <Grid container justifyContent="center" columns={{ xs: 4, sm: 8, md: 12 }}>
-          <Grid item xs={2} sm={3} md={5}>
-            <img
-              style={{ width: '100%' }}
-              src={essenciaisImg}
-              alt="WineBox Essenciais - Demonstrativo de garrafas do plano"
-            />
+          <Grid
+            container
+            justifyContent="center"
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            <Grid item xs={2} sm={3} md={5}>
+              <img
+                style={{ width: '100%' }}
+                src={essenciaisImg}
+                alt="WineBox Essenciais - Demonstrativo de garrafas do plano"
+              />
+            </Grid>
+            <Grid item xs={4} sm={5} md={7}>
+              <span className="winebox-title">WineBox</span>
+              <Typography
+                variant="h3"
+                component="h1"
+                sx={{ fontFamily: '"Sniglet", cursive', fontWeight: 700 }}
+              >
+                {subscription.name}
+              </Typography>
+              <p>{subscription.description}</p>
+              <Hidden smDown>
+                <Grid container spacing={4}>
+                  {subscription.plans.map((plan) => (
+                    <Grid item key={plan.name} sm={6} md={5}>
+                      <PlanCard selectedPlan={plan} />
+                    </Grid>
+                  ))}
+                </Grid>
+              </Hidden>
+            </Grid>
+            {subscription.plans && (
+              <Hidden smUp>
+                <SubscriptionToggle />
+              </Hidden>
+            )}
           </Grid>
-          <Grid item xs={4} sm={5} md={7}>
-            <span className="winebox-title">WineBox</span>
-            <h1>{subscription.name}</h1>
-            <p>{subscription.description}</p>
-            <Hidden smDown>
-              <Grid container spacing={4}>
-                {subscription.plans.map((plan) => (
-                  <Grid item key={plan.name} sm={6} md={5}>
-                    <PlanCard selectedPlan={plan} />
-                  </Grid>
-                ))}
-              </Grid>
-            </Hidden>
-          </Grid>
-          {subscription.plans && (
-            <Hidden smUp>
-              <SubscriptionToggle />
-            </Hidden>
-          )}
-        </Grid>
         )}
       </Container>
     </PlansContainerBox>
